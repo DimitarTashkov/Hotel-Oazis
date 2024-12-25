76630aacaa3bf26c64d03330d260b6884a946167
DimitarTashkov <mitkotashkov1979@gmail.com>
2024-12-25 19:29:45 +0200

    login form design

--- HotelOazis/Forms/Login.Designer.cs
+++ HotelOazis/Forms/Login.Designer.cs
@@ -29,14 +29,137 @@
         private void InitializeComponent()
         {
             formPanel = new Panel();
+            register = new Label();
+            loginBtn = new Button();
+            passwordField = new TextBox();
+            passwordLabel = new Label();
+            usernameField = new TextBox();
+            usernameLabel = new Label();
+            logo = new PictureBox();
+            usernameError = new Label();
+            passwordError = new Label();
+            formPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)logo).BeginInit();
             SuspendLayout();
             // 
             // formPanel
             // 
-            formPanel.Location = new Point(231, 44);
+            formPanel.BackColor = Color.White;
+            formPanel.Controls.Add(passwordError);
+            formPanel.Controls.Add(usernameError);
+            formPanel.Controls.Add(register);
+            formPanel.Controls.Add(loginBtn);
+            formPanel.Controls.Add(passwordField);
+            formPanel.Controls.Add(passwordLabel);
+            formPanel.Controls.Add(usernameField);
+            formPanel.Controls.Add(usernameLabel);
+            formPanel.Controls.Add(logo);
+            formPanel.Location = new Point(225, 50);
             formPanel.Name = "formPanel";
             formPanel.Size = new Size(350, 350);
-            formPanel.TabIndex = 0;
+            formPanel.TabIndex = 1;
+            // 
+            // register
+            // 
+            register.AutoSize = true;
+            register.BackColor = Color.Transparent;
+            register.Cursor = Cursors.Hand;
+            register.Font = new Font("Verdana", 9F, FontStyle.Bold | FontStyle.Italic | FontStyle.Underline);
+            register.ForeColor = Color.Blue;
+            register.Location = new Point(136, 315);
+            register.Name = "register";
+            register.Size = new Size(62, 14);
+            register.TabIndex = 6;
+            register.Text = "Register";
+            // 
+            // loginBtn
+            // 
+            loginBtn.BackColor = Color.SkyBlue;
+            loginBtn.Font = new Font("Verdana", 12F, FontStyle.Bold | FontStyle.Italic);
+            loginBtn.Location = new Point(90, 260);
+            loginBtn.Name = "loginBtn";
+            loginBtn.Size = new Size(160, 41);
+            loginBtn.TabIndex = 5;
+            loginBtn.Text = "Login";
+            loginBtn.UseVisualStyleBackColor = false;
+            loginBtn.Click += button1_Click;
+            // 
+            // passwordField
+            // 
+            passwordField.BackColor = Color.LightGray;
+            passwordField.Font = new Font("Verdana", 12F);
+            passwordField.ForeColor = Color.DimGray;
+            passwordField.Location = new Point(4, 197);
+            passwordField.Name = "passwordField";
+            passwordField.Size = new Size(346, 27);
+            passwordField.TabIndex = 4;
+            // 
+            // passwordLabel
+            // 
+            passwordLabel.AutoSize = true;
+            passwordLabel.BackColor = Color.Transparent;
+            passwordLabel.Font = new Font("Berlin Sans FB", 15.75F);
+            passwordLabel.Location = new Point(116, 171);
+            passwordLabel.Name = "passwordLabel";
+            passwordLabel.Size = new Size(92, 23);
+            passwordLabel.TabIndex = 3;
+            passwordLabel.Text = "Password";
+            // 
+            // usernameField
+            // 
+            usernameField.BackColor = Color.LightGray;
+            usernameField.Font = new Font("Verdana", 12F);
+            usernameField.ForeColor = Color.DimGray;
+            usernameField.Location = new Point(4, 115);
+            usernameField.Name = "usernameField";
+            usernameField.Size = new Size(346, 27);
+            usernameField.TabIndex = 2;
+            // 
+            // usernameLabel
+            // 
+            usernameLabel.AutoSize = true;
+            usernameLabel.BackColor = Color.Transparent;
+            usernameLabel.Font = new Font("Berlin Sans FB", 15.75F);
+            usernameLabel.Location = new Point(116, 89);
+            usernameLabel.Name = "usernameLabel";
+            usernameLabel.Size = new Size(98, 23);
+            usernameLabel.TabIndex = 1;
+            usernameLabel.Text = "Username";
+            // 
+            // logo
+            // 
+            logo.BackColor = Color.Transparent;
+            logo.Location = new Point(-21, -16);
+            logo.Name = "logo";
+            logo.Size = new Size(101, 94);
+            logo.TabIndex = 0;
+            logo.TabStop = false;
+            // 
+            // usernameError
+            // 
+            usernameError.AutoSize = true;
+            usernameError.BackColor = Color.Transparent;
+            usernameError.Font = new Font("Verdana", 8.25F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            usernameError.ForeColor = Color.Red;
+            usernameError.Location = new Point(7, 144);
+            usernameError.Name = "usernameError";
+            usernameError.Size = new Size(47, 13);
+            usernameError.TabIndex = 7;
+            usernameError.Text = "label1";
+            usernameError.Visible = false;
+            // 
+            // passwordError
+            // 
+            passwordError.AutoSize = true;
+            passwordError.BackColor = Color.Transparent;
+            passwordError.Font = new Font("Verdana", 8.25F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            passwordError.ForeColor = Color.Red;
+            passwordError.Location = new Point(7, 227);
+            passwordError.Name = "passwordError";
+            passwordError.Size = new Size(47, 13);
+            passwordError.TabIndex = 8;
+            passwordError.Text = "label1";
+            passwordError.Visible = false;
             // 
             // Login
             // 
@@ -49,11 +172,23 @@
             Name = "Login";
             Text = "Login";
             Load += Login_Load;
+            formPanel.ResumeLayout(false);
+            formPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)logo).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
 
         private Panel formPanel;
+        private Label register;
+        private Button loginBtn;
+        private TextBox passwordField;
+        private Label passwordLabel;
+        private TextBox usernameField;
+        private Label usernameLabel;
+        private PictureBox logo;
+        private Label usernameError;
+        private Label passwordError;
     }
 }
--- HotelOazis/Forms/Login.cs
+++ HotelOazis/Forms/Login.cs
@@ -1,4 +1,8 @@
-﻿using HotelOazis.Utilities;
+﻿using HotelOazis.Common.Constants;
+using HotelOazis.Utilities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Microsoft.VisualBasic.Logging;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -8,6 +12,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static HotelOazis.Common.Messages.ErrorMessages;
 
 namespace HotelOazis.Forms
 {
@@ -23,5 +28,57 @@ namespace HotelOazis.Forms
         {
 
         }
+
+        private void formPanel_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+        private void register_hover(object sender, EventArgs e)
+        {
+            register.ForeColor = Color.Indigo;
+        }
+
+        private void register_leave(object sender, EventArgs e)
+        {
+            register.ForeColor = Color.Blue;
+        }
+        private void register_Click(object sender, EventArgs e)
+        {
+            register.ForeColor = Color.BlueViolet;
+            Register registerForm = new Register();
+            Program.SwitchMainForm(registerForm);
+        }
+
+        private void login_hover(object sender, EventArgs e)
+        {
+            loginBtn.BackColor = Color.DeepSkyBlue;
+        }
+
+        private void login_leave(object sender, EventArgs e)
+        {
+            loginBtn.BackColor = Color.SkyBlue;
+            loginBtn.ForeColor = Color.Black;
+        }
+        private async void login_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void clearInputs_click(object sender, EventArgs e)
+        {
+            TextBox textbox = sender as TextBox;
+            textbox.Text = "";
+        }
+        private void input_TextChanged(object sender, EventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            textBox.ForeColor = Color.Black;
+            textBox.Font = FontsPicker.BaseFont;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
     }
 }

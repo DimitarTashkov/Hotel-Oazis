b09f40067a11cc905601a3ec8271acc569d2918c
DimitarTashkov <mitkotashkov1979@gmail.com>
2024-12-27 11:48:43 +0200

    add user service

---
+++ HotelOazis/Services/Interfaces/IUserService.cs
@@ -0,0 +1,16 @@
+﻿using HotelOazis.DTOs.User;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelOazis.Services.Interfaces
+{
+    public interface IUserService
+    {
+        public Task<(bool IsValid, List<ValidationResult> Errors)> ValidateLoginAsync(LoginUserInputModel loginModel);
+        public Task<bool> AuthenticateUserAsync(string username, string password);
+    }
+}
---
+++ HotelOazis/Services/UserService.cs
@@ -0,0 +1,39 @@
+﻿using HotelOazis.DTOs.User;
+using HotelOazis.Models.DbConfiguration;
+using HotelOazis.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelOazis.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly HotelContext _dbContext;
+
+        public UserService(HotelContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public async Task<bool> AuthenticateUserAsync(string username, string password)
+        {
+            return await _dbContext.Users
+                .AnyAsync(u => u.Username == username && u.Password == password);
+        }
+
+        public async Task<(bool IsValid, List<ValidationResult> Errors)> ValidateLoginAsync(LoginUserInputModel loginModel)
+        {
+            return await Task.Run(() =>
+            {
+                var validationResults = new List<ValidationResult>();
+                var validationContext = new ValidationContext(loginModel);
+                bool isValid = Validator.TryValidateObject(loginModel, validationContext, validationResults, true);
+                return (isValid, validationResults);
+            });
+        }
+    }
+}

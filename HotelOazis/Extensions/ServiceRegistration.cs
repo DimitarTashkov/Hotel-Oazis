a0f66f2099dc922908491dc48b37481db541c5d7
DimitarTashkov <mitkotashkov1979@gmail.com>
2025-01-05 15:18:47 +0200

    added external class for services registration and service dependency resolution

---
+++ HotelOazis/Extensions/ServiceRegistration.cs
@@ -0,0 +1,26 @@
+﻿using HotelOazis.Models.DbConfiguration;
+using HotelOazis.Services.Interfaces;
+using HotelOazis.Services;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelOazis.Extensions
+{
+    public static class ServiceRegistration
+    {
+        public static IServiceCollection AddHotelServices(this IServiceCollection services)
+        {
+            services.AddDbContext<HotelContext>(options =>
+                options.UseSqlServer(Configuration.ConnectionString));
+
+            services.AddScoped<IUserService, UserService>();
+            services.AddScoped<IRoomService, RoomService>();
+            return services;
+        }
+    }
+}

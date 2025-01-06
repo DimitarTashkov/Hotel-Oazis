94e428194ba2bf6143130f2f456fd17286446f7a
DimitarTashkov <mitkotashkov1979@gmail.com>
2025-01-07 00:17:19 +0200

    Implemented form handling room management and control

--- HotelOazis/Forms/EditRoom.Designer.cs
+++ HotelOazis/Forms/EditRoom.Designer.cs
@@ -28,12 +28,297 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "EditRoom";
+            formPanel = new Panel();
+            statusErrors = new Label();
+            statusBox = new ComboBox();
+            statusLabel = new Label();
+            imageErrors = new Label();
+            priceErrors = new Label();
+            typeErrors = new Label();
+            roomTypes = new ComboBox();
+            navigationButton = new Button();
+            roomPicture = new PictureBox();
+            uploadRoom = new Button();
+            uploadImage = new Button();
+            descriptionField = new TextBox();
+            descriptionLabel = new Label();
+            priceField = new TextBox();
+            priceLabel = new Label();
+            typeLabel = new Label();
+            logo = new PictureBox();
+            roomLabel = new Label();
+            formPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)roomPicture).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)logo).BeginInit();
+            SuspendLayout();
+            // 
+            // formPanel
+            // 
+            formPanel.BackColor = SystemColors.Control;
+            formPanel.Controls.Add(statusErrors);
+            formPanel.Controls.Add(statusBox);
+            formPanel.Controls.Add(statusLabel);
+            formPanel.Controls.Add(imageErrors);
+            formPanel.Controls.Add(priceErrors);
+            formPanel.Controls.Add(typeErrors);
+            formPanel.Controls.Add(roomTypes);
+            formPanel.Controls.Add(navigationButton);
+            formPanel.Controls.Add(roomPicture);
+            formPanel.Controls.Add(uploadRoom);
+            formPanel.Controls.Add(uploadImage);
+            formPanel.Controls.Add(descriptionField);
+            formPanel.Controls.Add(descriptionLabel);
+            formPanel.Controls.Add(priceField);
+            formPanel.Controls.Add(priceLabel);
+            formPanel.Controls.Add(typeLabel);
+            formPanel.Controls.Add(logo);
+            formPanel.Controls.Add(roomLabel);
+            formPanel.Location = new Point(71, 25);
+            formPanel.Name = "formPanel";
+            formPanel.Size = new Size(659, 400);
+            formPanel.TabIndex = 3;
+            // 
+            // statusErrors
+            // 
+            statusErrors.AutoSize = true;
+            statusErrors.BackColor = Color.Transparent;
+            statusErrors.Font = new Font("Verdana", 8.25F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            statusErrors.ForeColor = Color.Red;
+            statusErrors.Location = new Point(115, 302);
+            statusErrors.Name = "statusErrors";
+            statusErrors.Size = new Size(47, 13);
+            statusErrors.TabIndex = 25;
+            statusErrors.Text = "label1";
+            statusErrors.Visible = false;
+            // 
+            // statusBox
+            // 
+            statusBox.Font = new Font("Verdana", 9.75F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            statusBox.FormattingEnabled = true;
+            statusBox.Items.AddRange(new object[] { "True", "False" });
+            statusBox.Location = new Point(115, 275);
+            statusBox.Name = "statusBox";
+            statusBox.Size = new Size(244, 24);
+            statusBox.TabIndex = 24;
+            // 
+            // statusLabel
+            // 
+            statusLabel.AutoSize = true;
+            statusLabel.BackColor = Color.Transparent;
+            statusLabel.Font = new Font("Verdana", 12F, FontStyle.Bold | FontStyle.Italic);
+            statusLabel.Location = new Point(181, 251);
+            statusLabel.Name = "statusLabel";
+            statusLabel.Size = new Size(86, 18);
+            statusLabel.TabIndex = 23;
+            statusLabel.Text = "Available";
+            // 
+            // imageErrors
+            // 
+            imageErrors.AutoSize = true;
+            imageErrors.BackColor = Color.Transparent;
+            imageErrors.Font = new Font("Verdana", 8.25F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            imageErrors.ForeColor = Color.Red;
+            imageErrors.Location = new Point(451, 255);
+            imageErrors.Name = "imageErrors";
+            imageErrors.Size = new Size(47, 13);
+            imageErrors.TabIndex = 22;
+            imageErrors.Text = "label1";
+            imageErrors.Visible = false;
+            // 
+            // priceErrors
+            // 
+            priceErrors.AutoSize = true;
+            priceErrors.BackColor = Color.Transparent;
+            priceErrors.Font = new Font("Verdana", 8.25F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            priceErrors.ForeColor = Color.Red;
+            priceErrors.Location = new Point(115, 158);
+            priceErrors.Name = "priceErrors";
+            priceErrors.Size = new Size(47, 13);
+            priceErrors.TabIndex = 21;
+            priceErrors.Text = "label1";
+            priceErrors.Visible = false;
+            // 
+            // typeErrors
+            // 
+            typeErrors.AutoSize = true;
+            typeErrors.BackColor = Color.Transparent;
+            typeErrors.Font = new Font("Verdana", 8.25F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            typeErrors.ForeColor = Color.Red;
+            typeErrors.Location = new Point(115, 87);
+            typeErrors.Name = "typeErrors";
+            typeErrors.Size = new Size(47, 13);
+            typeErrors.TabIndex = 20;
+            typeErrors.Text = "label1";
+            typeErrors.Visible = false;
+            // 
+            // roomTypes
+            // 
+            roomTypes.Font = new Font("Verdana", 9.75F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            roomTypes.FormattingEnabled = true;
+            roomTypes.Location = new Point(115, 61);
+            roomTypes.Name = "roomTypes";
+            roomTypes.Size = new Size(244, 24);
+            roomTypes.TabIndex = 19;
+            // 
+            // navigationButton
+            // 
+            navigationButton.BackColor = Color.DarkOrange;
+            navigationButton.Font = new Font("Verdana", 11.25F, FontStyle.Bold | FontStyle.Italic);
+            navigationButton.Location = new Point(8, 318);
+            navigationButton.Name = "navigationButton";
+            navigationButton.Size = new Size(200, 45);
+            navigationButton.TabIndex = 18;
+            navigationButton.Text = "Return to rooms";
+            navigationButton.UseVisualStyleBackColor = false;
+            navigationButton.Click += navigationButton_Click;
+            // 
+            // roomPicture
+            // 
+            roomPicture.BackgroundImageLayout = ImageLayout.Stretch;
+            roomPicture.Location = new Point(451, 16);
+            roomPicture.Name = "roomPicture";
+            roomPicture.Size = new Size(200, 200);
+            roomPicture.SizeMode = PictureBoxSizeMode.StretchImage;
+            roomPicture.TabIndex = 17;
+            roomPicture.TabStop = false;
+            // 
+            // uploadRoom
+            // 
+            uploadRoom.BackColor = Color.DodgerBlue;
+            uploadRoom.Font = new Font("Verdana", 11.25F, FontStyle.Bold | FontStyle.Italic);
+            uploadRoom.Location = new Point(451, 318);
+            uploadRoom.Name = "uploadRoom";
+            uploadRoom.Size = new Size(200, 45);
+            uploadRoom.TabIndex = 15;
+            uploadRoom.Text = "Edit";
+            uploadRoom.UseVisualStyleBackColor = false;
+            uploadRoom.Click += editButton_Click;
+            // 
+            // uploadImage
+            // 
+            uploadImage.Font = new Font("Verdana", 9.75F, FontStyle.Bold | FontStyle.Italic);
+            uploadImage.Location = new Point(451, 222);
+            uploadImage.Name = "uploadImage";
+            uploadImage.Size = new Size(200, 30);
+            uploadImage.TabIndex = 13;
+            uploadImage.Text = "Upload image";
+            uploadImage.UseVisualStyleBackColor = true;
+            uploadImage.Click += uploadImage_click;
+            // 
+            // descriptionField
+            // 
+            descriptionField.BackColor = Color.LightGray;
+            descriptionField.Font = new Font("Verdana", 9.75F, FontStyle.Bold | FontStyle.Italic);
+            descriptionField.ForeColor = Color.DimGray;
+            descriptionField.Location = new Point(21, 222);
+            descriptionField.Name = "descriptionField";
+            descriptionField.Size = new Size(414, 23);
+            descriptionField.TabIndex = 12;
+            // 
+            // descriptionLabel
+            // 
+            descriptionLabel.AutoSize = true;
+            descriptionLabel.BackColor = Color.Transparent;
+            descriptionLabel.Font = new Font("Verdana", 12F, FontStyle.Bold | FontStyle.Italic);
+            descriptionLabel.Location = new Point(181, 176);
+            descriptionLabel.Name = "descriptionLabel";
+            descriptionLabel.Size = new Size(106, 18);
+            descriptionLabel.TabIndex = 11;
+            descriptionLabel.Text = "Description";
+            // 
+            // priceField
+            // 
+            priceField.BackColor = Color.LightGray;
+            priceField.Font = new Font("Verdana", 9.75F, FontStyle.Bold | FontStyle.Italic);
+            priceField.ForeColor = Color.DimGray;
+            priceField.Location = new Point(115, 132);
+            priceField.Name = "priceField";
+            priceField.Size = new Size(244, 23);
+            priceField.TabIndex = 8;
+            // 
+            // priceLabel
+            // 
+            priceLabel.AutoSize = true;
+            priceLabel.BackColor = Color.Transparent;
+            priceLabel.Font = new Font("Verdana", 12F, FontStyle.Bold | FontStyle.Italic);
+            priceLabel.Location = new Point(193, 106);
+            priceLabel.Name = "priceLabel";
+            priceLabel.Size = new Size(94, 18);
+            priceLabel.TabIndex = 7;
+            priceLabel.Text = "*Price(lv)";
+            // 
+            // typeLabel
+            // 
+            typeLabel.AutoSize = true;
+            typeLabel.BackColor = Color.Transparent;
+            typeLabel.Font = new Font("Verdana", 12F, FontStyle.Bold | FontStyle.Italic);
+            typeLabel.Location = new Point(193, 40);
+            typeLabel.Name = "typeLabel";
+            typeLabel.Size = new Size(63, 18);
+            typeLabel.TabIndex = 5;
+            typeLabel.Text = "*Type";
+            // 
+            // logo
+            // 
+            logo.BackColor = Color.Transparent;
+            logo.BackgroundImage = Properties.Resources.applogo1;
+            logo.BackgroundImageLayout = ImageLayout.Stretch;
+            logo.Image = Properties.Resources.applogo1;
+            logo.Location = new Point(-21, -16);
+            logo.Name = "logo";
+            logo.Size = new Size(150, 150);
+            logo.TabIndex = 1;
+            logo.TabStop = false;
+            // 
+            // roomLabel
+            // 
+            roomLabel.AutoSize = true;
+            roomLabel.BackColor = Color.Transparent;
+            roomLabel.Font = new Font("Verdana", 15.75F, FontStyle.Bold | FontStyle.Italic);
+            roomLabel.Location = new Point(270, 0);
+            roomLabel.Name = "roomLabel";
+            roomLabel.Size = new Size(130, 25);
+            roomLabel.TabIndex = 0;
+            roomLabel.Text = "Room info";
+            // 
+            // EditRoom
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = Properties.Resources.appback;
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(800, 450);
+            Controls.Add(formPanel);
+            Name = "EditRoom";
+            Text = "EditRoom";
+            Load += EditRoom_Load;
+            formPanel.ResumeLayout(false);
+            formPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)roomPicture).EndInit();
+            ((System.ComponentModel.ISupportInitialize)logo).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Panel formPanel;
+        private Label imageErrors;
+        private Label priceErrors;
+        private Label typeErrors;
+        private ComboBox roomTypes;
+        private Button navigationButton;
+        private PictureBox roomPicture;
+        private Button uploadRoom;
+        private Button uploadImage;
+        private TextBox descriptionField;
+        private Label descriptionLabel;
+        private TextBox priceField;
+        private Label priceLabel;
+        private Label typeLabel;
+        private PictureBox logo;
+        private Label roomLabel;
+        private Label statusErrors;
+        private ComboBox statusBox;
+        private Label statusLabel;
     }
 }
--- HotelOazis/Forms/EditRoom.cs
+++ HotelOazis/Forms/EditRoom.cs
@@ -1,5 +1,10 @@
-﻿using HotelOazis.DTOs.Room;
+﻿using HotelOazis.Common.Constants;
+using HotelOazis.DTOs.Room;
+using HotelOazis.Extensions;
+using HotelOazis.Models.Enumerations;
+using HotelOazis.Services;
 using HotelOazis.Services.Interfaces;
+using HotelOazis.Utilities;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -10,13 +15,154 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using static HotelOazis.Common.Messages.ResultMessages.RoomMessages;
+using static HotelOazis.Common.Messages.ErrorMessages.InputsMessages;
+using static HotelOazis.Common.Constants.ValidationConstants.InputConstants;
+using Fitness.Utilities;
+
+
+
 namespace HotelOazis.Forms
 {
     public partial class EditRoom : Form
     {
+        private readonly IRoomService roomService;
+        private readonly IUserService userService;
+        private readonly EditRoomInputModel model;
         public EditRoom(IRoomService roomService, EditRoomInputModel model)
         {
+            ActiveControl = priceLabel;
+            this.roomService = roomService;
+            this.model = model;
+            this.userService = ServiceLocator.GetService<IUserService>();
             InitializeComponent();
+            formPanel.Paint += new PaintEventHandler(LayoutHelper.set_FormBackground);
+
+            priceField.TextChanged += EventsEffects.input_TextChanged;
+            priceField.Click += EventsEffects.clearInputs_click;
+            descriptionField.TextChanged += EventsEffects.input_TextChanged;
+            descriptionField.Click += EventsEffects.clearInputs_click;
+
+        }
+
+        private void EditRoom_Load(object sender, EventArgs e)
+        {
+            roomTypes.DataSource = Enum.GetValues(typeof(RoomType));
+
+            roomTypes.SelectedItem = model.Type;
+            priceField.Text = model.Price.ToString();
+            roomPicture.ImageLocation = model.PictureLocation;
+            descriptionField.Text = model.Description;
+            statusBox.SelectedIndex = model.IsAvailable ? 0 : 1;
+
+            roomTypes.ForeColor = Color.DimGray;
+            priceField.ForeColor = Color.DimGray;
+            descriptionField.ForeColor = Color.DimGray;
+            statusBox.ForeColor = Color.DimGray;
+
+            roomTypes.Font = FontsPicker.DetailsFont;
+            priceField.Font = FontsPicker.DetailsFont;
+            descriptionField.Font = FontsPicker.DetailsFont;
+            statusBox.Font = FontsPicker.DetailsFont;
+
+            roomTypes.DropDownStyle = ComboBoxStyle.DropDownList;
+            statusBox.DropDownStyle = ComboBoxStyle.DropDownList;
+
+        }
+        private async void editButton_Click(object sender, EventArgs e)
+        {
+            List<TextBox> inputs = new List<TextBox>
+            {
+                priceField,
+                descriptionField,
+            };
+            bool areInputsValid = ValidationHelper.ValidateUserInputs(inputs, roomPicture);
+            if (!areInputsValid ||
+                roomTypes.SelectedItem == null || statusBox.SelectedItem == null)
+            {
+                MessageBox.Show(EmptyInputData, "Room Creation Failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            decimal unparsePrice = decimal.TryParse(priceField.Text, out var price) ? price : 0;
+
+            var editModel = new EditRoomInputModel()
+            {
+                Id = model.Id,
+                Type = (RoomType)roomTypes.SelectedItem,
+                Price = price,
+                PictureLocation = roomPicture.ImageLocation,
+                Description = descriptionField.Text,
+                IsAvailable = statusBox.SelectedIndex == 0 ? true : false
+            };
+
+            var (isValid, errors) = await userService.ValidateModelAsync(editModel);
+            if (!isValid)
+            {
+                foreach (var error in errors)
+                {
+                    if (error.MemberNames.Contains(nameof(editModel.Type)))
+                    {
+                        typeErrors.Text = error.ErrorMessage;
+                        typeErrors.Visible = true;
+                    }
+                    if (error.MemberNames.Contains(nameof(editModel.Price)))
+                    {
+                        priceErrors.Text = error.ErrorMessage;
+                        priceErrors.Visible = true;
+                    }
+                    if (error.MemberNames.Contains(nameof(editModel.PictureLocation)))
+                    {
+                        imageErrors.Text = error.ErrorMessage;
+                        imageErrors.Visible = true;
+                    }
+                    if (error.MemberNames.Contains(nameof(editModel.IsAvailable)))
+                    {
+                        statusErrors.Text = error.ErrorMessage;
+                        statusErrors.Visible = true;
+                    }
+                }
+                return;
+            }
+
+            bool success = await roomService.EditRoomAsync(editModel);
+            if (success)
+            {
+                MessageBox.Show(RoomUpdatedSuccessfully, "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                EditRoom editForm = new EditRoom(roomService, editModel);
+                Program.SwitchMainForm(editForm);
+            }
+            else
+            {
+                MessageBox.Show(RoomUpdateFailed, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void navigationButton_Click(object sender, EventArgs e)
+        {
+            Rooms roomsForm = new Rooms(roomService, userService);
+            Program.SwitchMainForm(roomsForm);
+        }
+        private void uploadImage_click(Object sender, EventArgs e)
+        {
+            String imageLocation = "";
+            try
+            {
+                OpenFileDialog dialog = new OpenFileDialog();
+                dialog.Filter = DialogFilter;
+
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    imageLocation = dialog.FileName;
+                }
+                roomPicture.ImageLocation = imageLocation;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(EmptyOrInvalidImage, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
         }
     }
 }

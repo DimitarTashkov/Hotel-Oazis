23d0d77b58a90ce7475f20f57ccf38e41d9c6a99
DimitarTashkov <mitkotashkov1979@gmail.com>
2025-01-08 20:06:21 +0200

    view models for service model

---
+++ HotelOazis/DTOs/Service/EditServiceInputModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using static HotelOazis.Common.Constants.ValidationConstants.ServiceConstants;
+
+
+namespace HotelOazis.DTOs.Service
+{
+    public class EditServiceInputModel
+    {
+        [Required]
+        public Guid Id { get; set; }
+
+        [Required]
+        [MaxLength(NameMaxLength)]
+        public string Name { get; set; } = null!;
+
+        [MaxLength(DescriptionMaxLength)]
+        public string Description { get; set; } = null!;
+    }
+}
---
+++ HotelOazis/DTOs/Service/ServiceViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelOazis.DTOs.Service
+{
+    public class ServiceViewModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Description { get; set; } = null!;
+    }
+}

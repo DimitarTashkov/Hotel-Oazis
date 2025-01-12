78b97067950fd9afea5f03a68d32eb92c88c066f
DimitarTashkov <mitkotashkov1979@gmail.com>
2025-01-12 22:55:41 +0200

    Created the base design for reservations.

--- HotelOazis/Forms/Reservations.Designer.cs
+++ HotelOazis/Forms/Reservations.Designer.cs
@@ -28,123 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            ordersContainer = new FlowLayoutPanel();
-            totalPriceHeaders = new FlowLayoutPanel();
-            userHeader = new Label();
-            addressHeader = new Label();
-            fromHeader = new Label();
-            toHeader = new Label();
-            daysHeader = new Label();
-            pricePerNightHeader = new Label();
-            arriveHeader = new Label();
+            reservationsContainer = new FlowLayoutPanel();
             ordersLabel = new Label();
             navigationButton = new Button();
-            ordersContainer.SuspendLayout();
-            totalPriceHeaders.SuspendLayout();
             SuspendLayout();
             // 
-            // ordersContainer
+            // reservationsContainer
             // 
-            ordersContainer.AutoScroll = true;
-            ordersContainer.Controls.Add(totalPriceHeaders);
-            ordersContainer.Location = new Point(20, 37);
-            ordersContainer.Name = "ordersContainer";
-            ordersContainer.Size = new Size(760, 400);
-            ordersContainer.TabIndex = 6;
-            // 
-            // totalPriceHeaders
-            // 
-            totalPriceHeaders.BackColor = Color.Cyan;
-            totalPriceHeaders.Controls.Add(userHeader);
-            totalPriceHeaders.Controls.Add(addressHeader);
-            totalPriceHeaders.Controls.Add(fromHeader);
-            totalPriceHeaders.Controls.Add(toHeader);
-            totalPriceHeaders.Controls.Add(daysHeader);
-            totalPriceHeaders.Controls.Add(pricePerNightHeader);
-            totalPriceHeaders.Controls.Add(arriveHeader);
-            totalPriceHeaders.Location = new Point(8, 8);
-            totalPriceHeaders.Margin = new Padding(8);
-            totalPriceHeaders.Name = "totalPriceHeaders";
-            totalPriceHeaders.Padding = new Padding(10);
-            totalPriceHeaders.Size = new Size(725, 32);
-            totalPriceHeaders.TabIndex = 0;
-            // 
-            // userHeader
-            // 
-            userHeader.AutoSize = true;
-            userHeader.Font = new Font("Verdana", 9.75F, FontStyle.Bold | FontStyle.Italic);
-            userHeader.Location = new Point(40, 10);
-            userHeader.Margin = new Padding(30, 0, 3, 0);
-            userHeader.Name = "userHeader";
-            userHeader.Size = new Size(40, 16);
-            userHeader.TabIndex = 0;
-            userHeader.Text = "User";
-            // 
-            // addressHeader
-            // 
-            addressHeader.AutoSize = true;
-            addressHeader.Font = new Font("Verdana", 9.75F, FontStyle.Bold | FontStyle.Italic);
-            addressHeader.Location = new Point(113, 10);
-            addressHeader.Margin = new Padding(30, 0, 3, 0);
-            addressHeader.Name = "addressHeader";
-            addressHeader.Size = new Size(49, 16);
-            addressHeader.TabIndex = 1;
-            addressHeader.Text = "Room";
-            // 
-            // fromHeader
-            // 
-            fromHeader.AutoSize = true;
-            fromHeader.Font = new Font("Verdana", 9.75F, FontStyle.Bold | FontStyle.Italic);
-            fromHeader.Location = new Point(195, 10);
-            fromHeader.Margin = new Padding(30, 0, 3, 0);
-            fromHeader.Name = "fromHeader";
-            fromHeader.Size = new Size(44, 16);
-            fromHeader.TabIndex = 2;
-            fromHeader.Text = "From";
-            // 
-            // toHeader
-            // 
-            toHeader.AutoSize = true;
-            toHeader.Font = new Font("Verdana", 9.75F, FontStyle.Bold | FontStyle.Italic);
-            toHeader.Location = new Point(277, 10);
-            toHeader.Margin = new Padding(35, 0, 3, 0);
-            toHeader.Name = "toHeader";
-            toHeader.Size = new Size(24, 16);
-            toHeader.TabIndex = 3;
-            toHeader.Text = "To";
-            // 
-            // daysHeader
-            // 
-            daysHeader.AutoSize = true;
-            daysHeader.Font = new Font("Verdana", 9.75F, FontStyle.Bold | FontStyle.Italic);
-            daysHeader.Location = new Point(339, 10);
-            daysHeader.Margin = new Padding(35, 0, 3, 0);
-            daysHeader.Name = "daysHeader";
-            daysHeader.Size = new Size(44, 16);
-            daysHeader.TabIndex = 4;
-            daysHeader.Text = "Days";
-            // 
-            // pricePerNightHeader
-            // 
-            pricePerNightHeader.AutoSize = true;
-            pricePerNightHeader.Font = new Font("Verdana", 9.75F, FontStyle.Bold | FontStyle.Italic);
-            pricePerNightHeader.Location = new Point(421, 10);
-            pricePerNightHeader.Margin = new Padding(35, 0, 3, 0);
-            pricePerNightHeader.Name = "pricePerNightHeader";
-            pricePerNightHeader.Size = new Size(111, 16);
-            pricePerNightHeader.TabIndex = 5;
-            pricePerNightHeader.Text = "Price per night";
-            // 
-            // arriveHeader
-            // 
-            arriveHeader.AutoSize = true;
-            arriveHeader.Font = new Font("Verdana", 9.75F, FontStyle.Bold | FontStyle.Italic);
-            arriveHeader.Location = new Point(585, 10);
-            arriveHeader.Margin = new Padding(50, 0, 3, 0);
-            arriveHeader.Name = "arriveHeader";
-            arriveHeader.Size = new Size(82, 16);
-            arriveHeader.TabIndex = 6;
-            arriveHeader.Text = "Total price";
+            reservationsContainer.AutoScroll = true;
+            reservationsContainer.Location = new Point(20, 37);
+            reservationsContainer.Name = "reservationsContainer";
+            reservationsContainer.Size = new Size(760, 400);
+            reservationsContainer.TabIndex = 6;
             // 
             // ordersLabel
             // 
@@ -169,6 +64,7 @@
             navigationButton.TabIndex = 7;
             navigationButton.Text = "Return to main";
             navigationButton.UseVisualStyleBackColor = false;
+            navigationButton.Click += navigationButton_Click;
             // 
             // Reservations
             // 
@@ -177,29 +73,19 @@
             BackgroundImage = Properties.Resources.appback;
             BackgroundImageLayout = ImageLayout.Stretch;
             ClientSize = new Size(800, 450);
-            Controls.Add(ordersContainer);
+            Controls.Add(reservationsContainer);
             Controls.Add(ordersLabel);
             Controls.Add(navigationButton);
             Name = "Reservations";
             Text = "Reservations";
-            ordersContainer.ResumeLayout(false);
-            totalPriceHeaders.ResumeLayout(false);
-            totalPriceHeaders.PerformLayout();
+            Load += Reservations_Load;
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
 
-        private FlowLayoutPanel ordersContainer;
-        private FlowLayoutPanel totalPriceHeaders;
-        private Label userHeader;
-        private Label addressHeader;
-        private Label fromHeader;
-        private Label toHeader;
-        private Label daysHeader;
-        private Label pricePerNightHeader;
-        private Label arriveHeader;
+        private FlowLayoutPanel reservationsContainer;
         private Label ordersLabel;
         private Button navigationButton;
     }
--- HotelOazis/Forms/Reservations.cs
+++ HotelOazis/Forms/Reservations.cs
@@ -1,4 +1,6 @@
-﻿using HotelOazis.Services.Interfaces;
+﻿using Fitness.Utilities;
+using HotelOazis.Common.Constants;
+using HotelOazis.Services.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -15,11 +17,137 @@ namespace HotelOazis.Forms
     {
         private readonly IUserService userService;
         private readonly IRoomService roomService;
+        private static bool isAuthorized;
         public Reservations(IUserService userService, IRoomService roomService)
         {
             this.userService = userService;
             this.roomService = roomService;
             InitializeComponent();
+        }
+
+        private async void Reservations_Load(object sender, EventArgs e)
+        {
+            //TODO: add icons to labels
+            var reservations = await roomService.GetReservationsAsync();
+
+            foreach (var reservation in reservations)
+            {
+                int daysReserved = (reservation.CheckOutDate - reservation.CheckInDate).Days;
+                decimal totalPrice = reservation.PricePerNight * daysReserved;
+
+                FlowLayoutPanel reservationContainer = new FlowLayoutPanel
+                {
+                    Name = $"reservationContainer_{reservation.Id}",
+                    Size = new Size(725, 120),
+                    Margin = new Padding(8),
+                    BackColor = Color.RosyBrown,
+                    FlowDirection = FlowDirection.TopDown,
+                    AutoSize = true,
+                    AutoSizeMode = AutoSizeMode.GrowAndShrink,
+                    WrapContents = false
+                };
+
+                RoundPictureBox userAvatar = new RoundPictureBox
+                {
+                    Name = $"userAvatar_{reservation.Id}",
+                    Size = new Size(50, 50),
+                    ImageLocation = reservation.PictureLocation,
+                    SizeMode = PictureBoxSizeMode.StretchImage,
+                    Anchor = AnchorStyles.Top | AnchorStyles.Left
+                };
+
+                reservationContainer.Controls.Add(userAvatar);
+
+                reservationContainer.Controls.Add(CreateLabel(
+                    $"Room {reservation.RoomNumber} ({reservation.RoomType})",
+                    FontsPicker.DetailsFont.Name,
+                    FontsPicker.DetailsFont.Size,
+                    FontStyle.Bold));
+
+                reservationContainer.Controls.Add(CreateLabel(
+                    $"Days Reserved: {daysReserved}",
+                    FontsPicker.DetailsFont.Name,
+                    FontsPicker.DetailsFont.Size));
+
+                reservationContainer.Controls.Add(CreateLabel(
+                    $"Total Price: ${totalPrice}",
+                    FontsPicker.DetailsFont.Name,
+                    FontsPicker.DetailsFont.Size));
+
+                reservationContainer.Controls.Add(CreateLabel(
+                    $"Check-in: {reservation.CheckInDate:yyyy-MM-dd} \nCheck-out: {reservation.CheckOutDate:yyyy-MM-dd}",
+                    FontsPicker.DetailsFont.Name,
+                    FontsPicker.DetailsFont.Size));
+
+                reservationContainer.Controls.Add(CreateLabel(
+                    $"Reserved by: {reservation.Username}",
+                    FontsPicker.DetailsFont.Name,
+                    FontsPicker.DetailsFont.Size));
+
+                Button cancelReservationButton = CreateButton(
+                    "Cancel",
+                    Color.Red,
+                    Color.White,
+                    100,
+                    30,
+                    $"cancelReservation_{reservation.Id}");
+                cancelReservationButton.Font = FontsPicker.DetailsFont;
+                cancelReservationButton.Anchor = AnchorStyles.None;
+                cancelReservationButton.Margin = new Padding(0);
+
+                cancelReservationButton.Click += async (s, ev) =>
+                {
+                    var confirm = MessageBox.Show("Are you sure you want to cancel this reservation?", "Confirm Cancellation", MessageBoxButtons.YesNo);
+                    if (confirm == DialogResult.Yes)
+                    {
+                        bool success = await roomService.CancelReservationAsync(reservation.Id);
+                        if (success)
+                        {
+                            MessageBox.Show("Reservation canceled successfully.");
+                            reservationsContainer.Controls.Remove(reservationContainer);
+                        }
+                        else
+                        {
+                            MessageBox.Show("Failed to cancel reservation. Please try again.");
+                        }
+                    }
+                };
+
+                reservationContainer.Controls.Add(cancelReservationButton);
+
+                reservationsContainer.Controls.Add(reservationContainer);
+            }
+        }
+        private Label CreateLabel(string text, string fontName, float fontSize, FontStyle fontStyle = FontStyle.Regular)
+        {
+            return new Label
+            {
+                Text = text,
+                Font = new Font(fontName, fontSize, fontStyle),
+                AutoSize = true,
+                Margin = new Padding(5),
+                TextAlign = ContentAlignment.MiddleCenter
+            };
+        }
+
+
+        private Button CreateButton(string text, Color backColor, Color foreColor, int width, int height, string name)
+        {
+            return new Button
+            {
+                Text = text,
+                BackColor = backColor,
+                ForeColor = foreColor,
+                Size = new Size(width, height),
+                Name = name,
+                Margin = new Padding(5)
+            };
+        }
+
+        private void navigationButton_Click(object sender, EventArgs e)
+        {
+            Index indexForm = new Index(userService);
+            Program.SwitchMainForm(indexForm);
         }
     }
 }
